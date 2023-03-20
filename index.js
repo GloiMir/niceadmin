85438cda08c11d70729d3f9cbc00f35bf1c62eba
GloiMir <gloirmirindi@gmail.com>
2023-03-20 12:38:36 +0200

    feat:publish htmls

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const __dirname = path.dirname(__filename);
 
 const app = express()
 
+app.use(express.static(path.join(__dirname,"./views")))
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, "views", "index.html"))
+    res.sendFile(path.join(__dirname, "./views/index.html"))
 })
 
 app.listen(4000,()=>console.log('Le serveur est en marche...'))
